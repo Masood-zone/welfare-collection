c4dcef34dbc962cf735daab42c37a5a6d51c41b3
masood <masoodacheampong@gmail.com>
2025-01-23 09:52:22 +0000

    fixed unused react errors

--- frontend/src/pages/auth/forgot-password/reset-password/reset-password.tsx
+++ frontend/src/pages/auth/forgot-password/reset-password/reset-password.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function ResetPassword() {
   return <div>ResetPassword</div>;
 }
--- frontend/src/pages/auth/forgot-password/verify-otp/verify-otp.tsx
+++ frontend/src/pages/auth/forgot-password/verify-otp/verify-otp.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function VerifyOTP() {
   return <div>VerifyOTP</div>;
 }
