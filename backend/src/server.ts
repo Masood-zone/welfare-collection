45fa5e426bc67a81ee47e013b81c7eaee8ef65c2
Clement Obeng <oklement3@gmail.com>
2025-01-25 22:58:42 +0000

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,10 +1,8 @@
-import http from "http";
 import app from "./app";
 
 const PORT = process.env.PORT || 8080;
 
-const httpServer = http.createServer(app);
 
-httpServer.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server started on port http://localhost:${PORT}`);
 });
