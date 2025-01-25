e2f156743df6837d2f9abb504e1af13c4dadfedf
masood <masoodacheampong@gmail.com>
2025-01-25 20:49:01 +0000

    changed api & removed unused react declarations

--- frontend/src/pages/admin/pages/payments/add/add-payment-form.tsx
+++ frontend/src/pages/admin/pages/payments/add/add-payment-form.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function AddPaymentForm() {
   return <div>AddPaymentForm</div>;
 }
--- frontend/src/pages/admin/pages/payments/add/add-payment.tsx
+++ frontend/src/pages/admin/pages/payments/add/add-payment.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 export default function AddPayment() {
   return <div>AddPayment</div>;
 }
