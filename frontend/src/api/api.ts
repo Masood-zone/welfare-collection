8984d0b7f1220009d18659df1e92c2921dcd6fbc
masood <masoodacheampong@gmail.com>
2025-01-25 23:18:20 +0000

    changed api route.

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -4,7 +4,8 @@ const token = user ? JSON.parse(user).token : "";
 
 export const api = axios.create({
   // baseURL: "http://localhost:8080/api/v1/web",
-  baseURL: "https://welfare-backend.onrender.com/api/v1/web",
+  // baseURL: "https://welfare-backend.onrender.com/api/v1/web",
+  baseURL: "https://welfare-backend.onrender.com/api/web",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
