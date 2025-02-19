520d2cb93d13301d617ca7ec9e2df17fb5b73707
masood <masoodacheampong@gmail.com>
2025-02-19 13:53:46 +0000

    changed api and .env files with prisma generation

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 const user = localStorage.getItem("user");
-const token = user ? JSON.parse(user).token : "";
+const userObj = user ? JSON.parse(user) : "";
+const token = userObj ? userObj.token : "";
 
 export const api = axios.create({
-  // baseURL: "http://localhost:8080/api/v1/web",
-  baseURL: "https://welfare-backend.onrender.com/api/v1/web",
+  baseURL: "http://localhost:8080/api/v1/web",
+  // baseURL: "https://welfare-backend.onrender.com/api/v1/web",
   headers: {
     "Access-Control-Allow-Origin": "*",
     "Content-Type": "application/json",
